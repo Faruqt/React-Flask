0c4e21c2240325e586604807eb7cde92f93c9aed
Faruq <faruqabdulsalam@yahoo.com>
2021-12-12 03:09:49 +0100

    Create axios get request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { useState } from 'react'
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+   // new line start
+  const [profileData, setProfileData] = useState(null)
+
+  function getData() {
+    axios({
+      method: "GET",
+      url:"/profile",
+    })
+    .then((response) => {
+      const res =response.data
+      setProfileData(({
+        profile_name: res.name,
+        about_me: res.about}))
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+        }
+    })}
+    //end of new line 
+    
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +42,15 @@ function App() {
         >
           Learn React
         </a>
+
+        {/* new line start*/}
+        <p>To get your profile details: </p><button onClick={getData}>Click me</button>
+        {profileData && <div>
+              <p>Profile name: {profileData.profile_name}</p>
+              <p>About me: {profileData.about_me}</p>
+            </div>
+        }
+         {/* end of new line */}
       </header>
     </div>
   );
